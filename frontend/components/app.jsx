e992e855cc1487a77cc20d9e1bec072891a2a9ec
echu18 <elizabethchu18@gmail.com>
2020-07-24 19:02:47 -0700

    add bio and links

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -8,13 +8,38 @@ import ArrangementContainer from './arrangement/arrangement_container';
 
 const App = () => (
   <div id="app">
-    <Link className='rewards-map' to={'/'}>Rewards Map</Link>
+    <Link className="rewards-map" to={"/"}>
+      Rewards Map
+    </Link>
     <div id="main">
       <Switch>
         <Route exact path="/" component={HomeContainer} />
         <Route exact path="/create" component={ArrangementContainer} />
-        <Route exact path="/arrangements/:arrangementId/edit" component={ArrangementContainer} />
+        <Route
+          exact
+          path="/arrangements/:arrangementId/edit"
+          component={ArrangementContainer}
+        />
       </Switch>
+
+      <div className="bio-container">
+        <p>
+          Demo project by{" "}
+          <a href="https://jamiechu.dev/" target="_blank">
+            Jamie Chu
+          </a>
+        </p>
+
+        <div className="bio-links">
+          <a href="https://github.com/echu18" target="_blank">
+            <i className="fa fa-github fa-2x" aria-hidden="true"></i>
+          </a>
+
+          <a href="https://www.linkedin.com/in/echu18/" target="_blank">
+            <i className="fa fa-linkedin fa-2x" aria-hidden="true"></i>
+          </a>
+        </div>
+      </div>
     </div>
   </div>
 );
